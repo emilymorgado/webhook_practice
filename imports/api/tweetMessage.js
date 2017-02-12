45c9362b456209dedfcd1b81ebbb93ae4f1bebe8
Emily Morgado <emilyannemorgado@gmail.com>
2017-02-12 12:16:23 -0800

    Adding input box, testing .gitignore

---
+++ imports/api/tweetMessage.js
@@ -0,0 +1,23 @@
+// import '../imports/ui/main.js';
+import '../../keys.json';
+
+
+// $.getJSON("../../keys.json", function(json) {
+//     console.log(json); // this will show the info it in firebug console 
+//     alert(json);
+// });
+
+// console.log('')
+// var api = twitter.Api(
+//     consumer_key=os.environ['TWITTER_CONSUMER_KEY'],
+//     consumer_secret=os.environ['TWITTER_CONSUMER_SECRET'],
+//     access_token_key=os.environ['TWITTER_ACCESS_TOKEN_KEY'],
+//     access_token_secret=os.environ['TWITTER_ACCESS_TOKEN_SECRET'])
+
+// # This will print info about credentials to make sure they're correct
+// print api.VerifyCredentials()
+
+// # Send a tweet
+// tweet = TweetableMarkovGenerator()
+// status = api.PostUpdate(tweet.make_text(tweet.make_chains(argv)))
+// print status.text
--- server/main.js
+++ server/main.js
@@ -1,4 +1,5 @@
 import { Meteor } from 'meteor/meteor';
+import '../imports/api/tweetMessage.js';
 
 Meteor.startup(() => {
   // code to run on server at startup
